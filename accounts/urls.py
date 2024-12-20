44fdf9c9815ea1125bed1b002dc41cce3e5b0771
Shubham Nirmal <suvamniraml456@gmail.com>
2024-12-20 16:55:07 +0545

    add URL routing for register form

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import DashboardView
+from .views import DashboardView, RegisterView
 
 urlpatterns = [
     path('', DashboardView.as_view(), name='dashboard'), # Dashboard
+    path('register/', RegisterView.as_view(), name='register'), # Register
 ]
