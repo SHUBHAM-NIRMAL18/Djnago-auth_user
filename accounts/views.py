366e4c8c54df4032d71dd7e80577c3d81203f889
Shubham Nirmal <suvamniraml456@gmail.com>
2024-12-20 16:50:22 +0545

    feat: add RegisterView to 'accounts' app views.py

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
 from django.views.generic import TemplateView, CreateView
+from django.urls import reverse_lazy
 
 
 # Create your views here.
 # View for Dashboard
 class DashboardView(LoginRequiredMixin, TemplateView):
     template_name = 'accounts/dashboard.html'
+
+
+# User Register View
+class RegisterView(CreateView):
+    template_name = 'accounts/register.html'
+    form_class = UserCreationForm # Built-in Register form
+    success_url = reverse_lazy('login') # Redirect to login page after register
+
