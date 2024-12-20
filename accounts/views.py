bf9f97b85092dab95382819af8610c522e476d44
Shubham Nirmal <suvamniraml456@gmail.com>
2024-12-20 16:26:39 +0545

    feat: add DashboardView to 'accounts' app views.py

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import TemplateView, CreateView
+
 
 # Create your views here.
+# View for Dashboard
+class DashboardView(LoginRequiredMixin, TemplateView):
+    template_name = 'accounts/dashboard.html'
